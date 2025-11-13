1eaa69ce95e5c6acf7aba98309479e7edc09d598
ALAN VITAS <49319580@ort>
2025-11-13 15:15:17 -0300

    Login, sessions, verCalendario

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,85 +15,25 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View("login");
+        return View("Login");
     }
 
     [HttpPost]
     public IActionResult verCalendario(DateTime fecha)
     {
         ViewBag.Fecha = fecha;
-        ViewBag.Recetas = new List<CalendariosxRecetas>()
-        {
-            new CalendariosxRecetas(
-                new Receta("Tostadas integrales con palta", "Desayuno saludable y rápido"),
-                new DateTime(2025, 11, 6),
-                "desayuno"
-            ),
-            new CalendariosxRecetas(
-                new Receta("Ensalada César", "Pollo, lechuga, crutones y aderezo ligero"),
-                new DateTime(2025, 11, 4),
-                "almuerzo"
-            ),
-            new CalendariosxRecetas(
-                new Receta("Sopa de calabaza", "Ideal para una cena liviana y nutritiva"),
-                new DateTime(2025, 11, 4),
-                "cena"
-            ),
-            new CalendariosxRecetas(
-                new Receta("Panqueques de avena", "Rápidos, ricos y con banana"),
-                new DateTime(2025, 11, 5),
-                "desayuno"
-            ),
-            new CalendariosxRecetas(
-                new Receta("Wraps de pollo", "Tortilla integral con vegetales y pollo grillado"),
-                new DateTime(2025, 11, 5),
-                "almuerzo"
-            ),
-            new CalendariosxRecetas(
-                new Receta("Salteado de verduras", "Liviano y lleno de sabor"),
-                new DateTime(2025, 11, 6),
-                "cena"
-            )
-        };
+        Usuario usuario = Objeto.StringToObject<Usuario>(HttpContext.Session.GetString("Usuario"));
+        int idUsuario = BD.buscarIdUsuario(usuario.email, usuario.contraseña);        
+        ViewBag.Recetas = BD.buscarCalendariosRecetas(idUsuario);
+
         return View("verCalendario");    
     }
 
     public IActionResult verCalendario()
     {
-        ViewBag.Fecha = DateTime.Today;
-        ViewBag.Recetas = new List<CalendariosxRecetas>()
-                {
-                    new CalendariosxRecetas(
-                        new Receta("Tostadas integrales con palta", "Desayuno saludable y rápido"),
-                        new DateTime(2025, 11, 6),
-                        "desayuno"
-                    ),
-                    new CalendariosxRecetas(
-                        new Receta("Ensalada César", "Pollo, lechuga, crutones y aderezo ligero"),
-                        new DateTime(2025, 11, 4),
-                        "almuerzo"
-                    ),
-                    new CalendariosxRecetas(
-                        new Receta("Sopa de calabaza", "Ideal para una cena liviana y nutritiva"),
-                        new DateTime(2025, 11, 4),
-                        "cena"
-                    ),
-                    new CalendariosxRecetas(
-                        new Receta("Panqueques de avena", "Rápidos, ricos y con banana"),
-                        new DateTime(2025, 11, 5),
-                        "desayuno"
-                    ),
-                    new CalendariosxRecetas(
-                        new Receta("Wraps de pollo", "Tortilla integral con vegetales y pollo grillado"),
-                        new DateTime(2025, 11, 5),
-                        "almuerzo"
-                    ),
-                    new CalendariosxRecetas(
-                        new Receta("Salteado de verduras", "Liviano y lleno de sabor"),
-                        new DateTime(2025, 11, 6),
-                        "cena"
-                    )
-                };
+        Usuario usuario = Objeto.StringToObject<Usuario>(HttpContext.Session.GetString("Usuario"));
+        int idUsuario = BD.buscarIdUsuario(usuario.email, usuario.contraseña);        
+        ViewBag.Recetas = BD.buscarCalendariosRecetas(idUsuario);
         return View("verCalendario");
     }
 
@@ -105,12 +45,14 @@ public class HomeController : Controller
         HttpContext.Session.SetString("Usuario", Objeto.ObjectToString<Usuario>(usu));
         return View("");
     }
-
+    
+    [HttpPost]
     public IActionResult Login(string email, string contraseña)
     {
         Usuario usuario = BD.buscarUsuario(email, contraseña);
         HttpContext.Session.SetString("Usuario", Objeto.ObjectToString<Usuario>(usuario));
-        return View("");
+        ViewBag.Usuario = usuario;
+        return View("Index");
     }
 
     public IActionResult AgregarCalendarioyReceta(DateTime fecha, string momento, string nombreReceta)
